1f34d900eff39988420c3c059a9304fef37ef0f4
SergeyVasiliyev <82042454+SergeyVasiliyev@users.noreply.github.com>
2024-02-14 13:06:05 +0300

    2.c

--- 2.c
+++ 2.c
@@ -1,10 +1,11 @@
-void sort(int *a, int n){
-    int i, j, step;
+void sort(int *a, int n){ // сортировка Шелла
+    int i, j, step; // берем шаги в степени двойки, каждый раз уменьшаем в раза
     int tmp;
     for (step = n / 2; step > 0; step /= 2)
         for (i = step; i < n; i++)
         {
             tmp = a[i];
+            // проходим по массиву, сравнивая элементы, разность которых равна шагу
             for (j = i; j >= step; j -= step)
             {
                 if (tmp < a[j - step])
